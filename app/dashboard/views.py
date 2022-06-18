65489486cebca60575cb71c86b48d48d9ecda3c6
macbookpro <emrefikirlier@gmail.com>
2022-06-18 23:33:56 +0300

    "Tempalte"

--- app/dashboard/views.py
+++ app/dashboard/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def dashboard(request):
+
+    response = render(request, 'raccoon_analytic/pages/dashboard.html')
+
+    return response
