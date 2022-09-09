81a756992ab1cf6eb30bfca952c0c44b0233bde7
macbookpro <emrefikirlier@gmail.com>
2022-09-09 20:21:41 +0300

    "aut ve email problemi düzelttim"

--- app/app/settings.py
+++ app/app/settings.py
@@ -22,11 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "hyRGwson7kwBiDm1h0yJpJu7Vtdjw7Nu2I4"
 #SECRET_KEY = os.environ.get('SECRET_KEY')
-print("\n\n\n SECRET KEY : {} \n\n\n".format(SECRET_KEY))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG', 0)))
-print("\n\n\n DEBUG STATUS: {} \n\n\n".format(DEBUG))
 
 ALLOWED_HOSTS = []
 ALLOWED_HOSTS.extend(
--- app/authentication/views.py
+++ app/authentication/views.py
@@ -7,31 +7,35 @@ from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 from layout.layout_service import LayoutService
 import time
+from vendor.email.email import send_mail, templates
+from django.conf import settings
 
 # Create your views here.
 
-def login(request, confirm = None):
+def login(request, verified_code = None):
 
     # İlk confirmeden sonra kullanıcının deneme süresi 
     # başlar accountda expire dateyi set et ve dashboarddaki notificationada gönder.
     context = {}
 
+    if verified_code:
+
+        verified_user = UserService().user_comfirmed(verified_code=verified_code)
+
+        if verified_user is False:
+            context['error_message'] = "Your account is not verified contact your administrator."
+
     if request.POST:
 
-        user = UserService().login_user(
-            email = request.POST['email'],
-            password = request.POST['password']
-        )
+        user = UserService().login_user(email = request.POST['email'], password = request.POST['password'])
 
         if user[0]:
             
             auth_login(request, user[1])
             menu = LayoutService().get_menues(user[1])
             Authantication.getInstance().setUser(user[1].id)
-            # time sleep 2
+            #response.set_cookie('username', user[1].username)
             response = redirect('dashboard:activity', menu[0]["menu"][0]["main_menu"])
-            response.set_cookie('username', user[1].username)
-            response.set_cookie('user_id', user[1].id)
             
             return response
         
@@ -72,19 +76,15 @@ def create_account(request, package=None):
 
 def logout(request):
     
-    user = Authantication.getInstance().getUser()
 
-    if UserService().logout_user(user):
+    if UserService().logout_user(request.user):
 
         auth_logout(request)
         response = redirect('authentication:login')
-        response.delete_cookie('username')
-        response.delete_cookie('user_id')
-        Authantication.getInstance().logutInstance()
         return response
     
     else:
-        print("Burada hata var incele...", user)
+        print("Burada hata var incele...",)
 
 
 def reset_password(request):
@@ -92,9 +92,10 @@ def reset_password(request):
     if request.POST:
         user = UserService().get_user_by_email(request.POST['email'])
         account = AccountService().get_user_account(user=user)
+        confirmed_url = "{}/auth/new_password/".format(request.META["HTTP_HOST"])
+        confirmed_url = confirmed_url + account.verified_code
         
-        time.sleep(5)
-        # Mail at
+        send_mail(subject="Reset Password",from_address=settings.EMAIL_HOST_USER, to_address=request.POST['email'], template="raccoon_analytic/email/password-reset.html", confirm_code=confirmed_url)
     
     response = render(request, 'raccoon_analytic/pages/password_reset.html')
 
